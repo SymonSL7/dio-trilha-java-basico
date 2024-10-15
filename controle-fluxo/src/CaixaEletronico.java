5c98a131b8f49cd18e64fb7d193c22e2d0d1a10e
Symon Lima <symonlima07@gmail.com>
2024-10-15 14:21:47 -0300

    Condicional Simples

---
+++ controle-fluxo/src/CaixaEletronico.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CaixaEletronico {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        double saldo, valorSolicitado;
+
+        saldo = 25.0;
+
+        System.out.println("Digite o valor à sacar:");
+        valorSolicitado = scanner.nextDouble();
+
+            saldo = saldo - valorSolicitado;
+
+            System.out.println("Saque efetuado com sucesso, seu saldo atual é: " + saldo);
+        } else {
+            System.err.println("Não foi possível efetuar o saque, pois seu saldo atual é: " + saldo);
+        }
+
+
+    }
+
+}
