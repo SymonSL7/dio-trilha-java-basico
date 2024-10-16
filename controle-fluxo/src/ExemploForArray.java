e787be8de3f37692be5fc6c4c2b8de59fb7119e6
Symon Lima <symonlima07@gmail.com>
2024-10-16 19:19:59 -0300

    Estrutura de Repetição.

---
+++ controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,15 @@
+public class ExemploForArray {
+
+    public static void main(String[] args) {
+        
+        String alunos [] = {"Symon", "Cida", "Jair", "Pietro"};
+
+        for (int x = 0 ; x < alunos.length ; x++){
+
+            System.out.println("O aluno no indice " + x + " é " + alunos[x]);
+
+        }
+
+    }
+    
+}
---
+++ controle-fluxo/src/ExemploForArray2.java
@@ -0,0 +1,15 @@
+public class ExemploForArray2 {
+
+    public static void main(String[] args) {
+        
+        String alunos [] = {"Symon", "Cida", "Jair", "Pietro"};
+
+        for (String aluno : alunos ){
+
+            System.out.println("Nome do aluno é: " + aluno);
+
+        }
+
+    }
+    
+}
